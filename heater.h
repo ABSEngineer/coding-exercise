32f42907e04be2e5a3d18802b3e285fdb944e291
Andrew <andrew.blower@outlook.com>
2023-08-13 20:26:32 +0100

    Added heater header

---
+++ heater.h
@@ -0,0 +1,48 @@
+#ifndef HEATER_H
+#define HEATER_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * The purpose of this function is to start the heater
+*/
+bool start(const char*, const size_t);
+
+/**
+ * The purpose of this function is to stop the heater
+*/
+bool stop();
+
+/**
+ * The purpose of this function is to return if the heater has started
+*/
+bool has_started();
+
+/**
+ * This purpose of this method is to output the state of the heater
+*/
+void* output();
+
+/**
+ * The purpose of this function is to update the heaters schedule
+*/
+bool update_schedule(const char*, size_t);
+
+/**
+ * The purpose of this method is to validate the updated schedule
+*/
+bool validate_schedule(const char*, const size_t, const size_t);
+
+/**
+ * The purpose of this method is to validate the size of the schedule
+*/
+bool validate_schedule_size(const size_t, const size_t);
+
+/**
+ * The purpose of this method is to validate the states of the schedule
+*/
+bool validate_schedule_states(const char*, size_t);
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,11 @@
+/**
+ * Started at 13/08/2023 - 20:15PM
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "heater.h"
+
+int main() {
+    return 0;
+}
+
